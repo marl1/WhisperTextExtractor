856960e56e49ac9019a2fbc45d01baba214260fd
MarcLv <levequemc@gmail.com>
2024-05-20 20:36:30 +0800

    Made linux script under linux to avoid 'failed to execve' error.

--- scripts/linux/whisperTextExtractor.sh
+++ scripts/linux/whisperTextExtractor.sh
@@ -1,1 +1,10 @@
-#!/bin/bash# Get the directory of the current scriptSCRIPT_DIR="$(dirname "$(readlink -f "$0")")"chmod +x "$SCRIPT_DIR/bin/java" # Execute the Java application"$SCRIPT_DIR/bin/java" -jar "$SCRIPT_DIR/jars/whisperTextExtractor-0.0.1-SNAPSHOT.jar"# Exit the scriptexit 0
+#!/bin/bash
+# Get the directory of the current script
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+
+chmod +x "$SCRIPT_DIR/bin/java" 
+# Execute the Java application
+"$SCRIPT_DIR/bin/java" -jar "$SCRIPT_DIR/jars/whisperTextExtractor-0.0.1-SNAPSHOT.jar"
+
+# Exit the script
+exit 0
