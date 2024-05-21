ca39480d49c357769bcf1f4783634abb9ccc7793
MarcLv <levequemc@gmail.com>
2024-05-21 14:51:02 +0800

    LICENCE + bump to 1.0.0 version.

--- scripts/windows/whisperTextExtractor.bat
+++ scripts/windows/whisperTextExtractor.bat
@@ -1,1 +1,1 @@
-start "" "%~dp0\bin\javaw" -jar "%~dp0\jars\whisperTextExtractor-0.0.1-SNAPSHOT-jar-with-dependencies.jar"  && exit 0
+start "" "%~dp0\bin\javaw" -jar "%~dp0\jars\whisperTextExtractor-1.0.0-jar-with-dependencies.jar"  && exit 0
